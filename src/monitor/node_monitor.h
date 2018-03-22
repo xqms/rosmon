9afc069f4e7619f5be3a938f5479f71469edd458
Max Schwarz <max.schwarz@online.de>
2018-03-22 13:20:16 +0100

    node_monitor: clean up and document

--- src/monitor/node_monitor.h
+++ src/monitor/node_monitor.h
@@ -24,14 +24,101 @@ public:
 	typedef std::shared_ptr<NodeMonitor> Ptr;
 	typedef std::shared_ptr<NodeMonitor> ConstPtr;
 
+	//! Process state
 	enum State
 	{
-		STATE_IDLE,
-		STATE_RUNNING,
-		STATE_CRASHED,
-		STATE_WAITING
+		STATE_IDLE,    //!< Idle (e.g. exited with code 0)
+		STATE_RUNNING, //!< Running
+		STATE_CRASHED, //!< Crashed (i.e. exited with code != 0)
+		STATE_WAITING  //!< Waiting for automatic restart after crash
 	};
 
+	/**
+	 * @brief Constructor
+	 *
+	 * @param launchNode Corresponding launch::Node instance
+	 * @param fdWatcher FDWatcher instance to register in
+	 * @param nh ros::NodeHandle to use for creating timers
+	 **/
+	NodeMonitor(
+		const launch::Node::ConstPtr& launchNode,
+		const FDWatcher::Ptr& fdWatcher, ros::NodeHandle& nh);
+	~NodeMonitor();
+
+	//! @name Starting & stopping
+	//@{
+
+	//! Start the node
+	void start();
+
+	//! Stop the node
+	void stop(bool restart = false);
+
+	//! Restart the node
+	void restart();
+
+	/**
+	 * @brief Start shutdown sequence
+	 *
+	 * If the node is still running, this sends SIGINT.
+	 **/
+	void shutdown();
+
+	/**
+	 * @brief Finish shutdown sequence
+	 *
+	 * If the node is still running, this sends SIGKILL and prints a warning.
+	 **/
+	void forceExit();
+
+	//! Is the node running?
+	bool running() const;
+
+	//! Get process state
+	State state() const;
+	//@}
+
+	//! @name Debugging
+	//@{
+
+	//! Is a core dump available from a crash under rosmon control?
+	inline bool coredumpAvailable() const
+	{ return !m_debuggerCommand.empty(); }
+
+	/**
+	 * @brief What command should we use to debug the coredump?
+	 *
+	 * @sa coredumpAvailable()
+	 **/
+	inline std::string debuggerCommand() const
+	{ return m_debuggerCommand; }
+
+	/**
+	 * @brief Launch gdb interactively
+	 *
+	 * This opens gdb in a new terminal window. If X11 is not available, the
+	 * gdb command is emitted via logMessageSignal().
+	 *
+	 * If a coredump is available (see coredumpAvailable()), gdb is launched
+	 * against the coredump instead of the running process.
+	 **/
+	void launchDebugger();
+	//@}
+
+	//! Node name
+	inline std::string name() const
+	{ return m_launchNode->name(); }
+
+	/**
+	 * @brief Logging signal
+	 *
+	 * Contains a log message (node name, message) to be printed or saved in a log file.
+	 **/
+	boost::signals2::signal<void(std::string,std::string)> logMessageSignal;
+
+	//! Signalled whenever the process exits.
+	boost::signals2::signal<void(std::string)> exitedSignal;
+private:
 	enum Command
 	{
 		CMD_RUN,
@@ -39,42 +126,10 @@ public:
 		CMD_RESTART
 	};
 
-	NodeMonitor(
-		const launch::Node::ConstPtr& launchNode,
-		const FDWatcher::Ptr& fdWatcher, ros::NodeHandle& nh);
-	~NodeMonitor();
-
 	std::vector<std::string> composeCommand() const;
-
-	void start();
-	void stop(bool restart = false);
-	void restart();
-
-	void shutdown();
-	void forceExit();
-
-	bool running() const;
-	State state() const;
 
 	void communicate();
 
-	inline int fd()
-	{ return m_fd; }
-
-	inline bool coredumpAvailable() const
-	{ return !m_debuggerCommand.empty(); }
-
-	inline std::string debuggerCommand() const
-	{ return m_debuggerCommand; }
-
-	void launchDebugger();
-
-	inline std::string name() const
-	{ return m_launchNode->name(); }
-
-	boost::signals2::signal<void(std::string,std::string)> logMessageSignal;
-	boost::signals2::signal<void(std::string)> exitedSignal;
-private:
 	void log(const char* fmt, ...) __attribute__ (( format (printf, 2, 3) ));
 	void checkStop();
 	void gatherCoredump(int signal);
