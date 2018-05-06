bc7a9f08283d4ffca2a94d31e6709aa0c47667dc
Max Schwarz <max.schwarz@online.de>
2018-05-06 02:24:27 +0200

    test: xml: top-level tests

---
+++ test/xml/test_basic.cpp
@@ -0,0 +1,43 @@
+// Unit tests for basic loading
+// Author: Max Schwarz <max.schwarz@ais.uni-bonn.de>
+
+#include <catch_ros/catch.hpp>
+
+#include "../../src/launch/launch_config.h"
+
+#include "param_utils.h"
+
+using namespace rosmon::launch;
+
+TEST_CASE("basic", "[basic]")
+{
+	LaunchConfig config;
+	config.parseString(R"EOF(
+		<launch>
+		</launch>
+	)EOF");
+}
+
+TEST_CASE("basic: invalid XML", "[basic]")
+{
+	LaunchConfig config;
+	REQUIRE_THROWS_AS(
+		config.parseString(R"EOF(
+			<launch><abc>
+			</launch>
+		)EOF"),
+		LaunchConfig::ParseException
+	);
+}
+
+TEST_CASE("basic: top-level attributes", "[basic]")
+{
+	LaunchConfig config;
+	config.parseString(R"EOF(
+		<launch rosmon-name="my_name" rosmon-window-title="my_title">
+		</launch>
+	)EOF");
+
+	CHECK(config.rosmonNodeName() == "my_name");
+	CHECK(config.windowTitle() == "my_title");
+}
