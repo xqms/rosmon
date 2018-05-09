ffdae6f8b5e449b5c3a830ae93de1a7be5b746db
Max Schwarz <max.schwarz@ais.uni-bonn.de>
2018-05-09 17:59:07 +0200

    test: xml: test <include>

---
+++ test/xml/test_include.cpp
@@ -0,0 +1,48 @@
+// Unit tests for include tags
+// Author: Max Schwarz <max.schwarz@ais.uni-bonn.de>
+
+#include <catch_ros/catch.hpp>
+
+#include "../../src/launch/launch_config.h"
+
+#include "param_utils.h"
+
+using namespace rosmon::launch;
+
+TEST_CASE("include basic", "[include]")
+{
+	LaunchConfig config;
+	config.parseString(R"EOF(
+		<launch>
+			<arg name="test_argument" value="hello" />
+
+			<include file="$(find rosmon)/test/basic_sub.launch">
+				<arg name="test_argument" value="$(arg test_argument)" />
+			</include>
+		</launch>
+	)EOF");
+
+	config.evaluateParameters();
+
+	auto params = config.parameters();
+
+	CHECK(getTypedParam<std::string>(params, "/test_argument") == "hello");
+}
+
+TEST_CASE("include pass_all", "[include]")
+{
+	LaunchConfig config;
+	config.parseString(R"EOF(
+		<launch>
+			<arg name="test_argument" value="hello" />
+
+			<include file="$(find rosmon)/test/basic_sub.launch" pass_all_args="true" />
+		</launch>
+	)EOF");
+
+	config.evaluateParameters();
+
+	auto params = config.parameters();
+
+	CHECK(getTypedParam<std::string>(params, "/test_argument") == "hello");
+}
