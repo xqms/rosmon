b744e79929f1f447411761fab199225a9b892e76
Max Schwarz <max.schwarz@ais.uni-bonn.de>
2020-10-14 18:04:33 +0200

    rosmon_core: env-hooks: follow symbolic links during file name suggestion
    This fixes bash completion for colcon linked install spaces.

--- rosmon_core/env-hooks/50-rosmon.bash
+++ rosmon_core/env-hooks/50-rosmon.bash
@@ -68,7 +68,7 @@ function _mon() {
 			# If we have no launch file yet, offer launch files
 			elif [[ ( ! -f $package_name ) && -z $launch_name ]]; then
 				local package_dir="$(rospack find ${COMP_WORDS[2]})"
-				local launchfiles=$(find "$package_dir" -name '*.launch' -type f -printf "%f\n")
+				local launchfiles=$(find -L "$package_dir" -name '*.launch' -type f -printf "%f\n")
 				COMPREPLY=( $(compgen -W "${launchfiles} ${FLAGS[*]} ${OPTS[*]}" -- $cur) )
 
 			# Only arguments now
